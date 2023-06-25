817c5f1eb65ce46a6e2ed790929967545a45c03d
Muktita <muktitakim@csu.fullerton.edu>
2023-06-25 01:26:57 -0700

    added firebase login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,22 @@
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableOpacityBase, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState }from 'react'
+import { auth } from '../firebase'
+
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignUp = () => {
+        auth
+            .createUserWithEmailAndPassword(email,password)
+            .then(userCredentials => {
+                const user = userCredentials.user;
+                console.log(user.email);
+            })
+            .catch(error => alert(error.message))
+    }
+
   return (
     <KeyboardAvoidingView
         style={styles.container}
@@ -10,14 +25,14 @@ const LoginScreen = () => {
       <View style={styles.inputContainer}>
           <TextInput
             placeholder='Email'
-            // value={ }
-            // onChangeText= {text => }
+            value={ email }
+            onChangeText= {text =>  setEmail(text)}
             style={styles.input}
             />
             <TextInput
             placeholder='Password'
-            // value={ }
-            // onChangeText= {text => }
+            value={password}
+            onChangeText= {text => setPassword(text)}
             style={styles.input}
             secureTextEntry
             />
@@ -31,7 +46,7 @@ const LoginScreen = () => {
               <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
           <TouchableOpacity
-                onPress={() => {}}
+                onPress={handleSignUp}
                 style={[styles.button, styles.buttonOutline]}
             >
               <Text style={styles.buttonOutlineText}>Register</Text>
